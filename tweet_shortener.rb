0964a90f7d22b306b1274cfd813542adba78a8e0
Samuel Prada <jspradac@gmail.com>
2018-11-09 19:25:29 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -4,11 +4,11 @@ def dictionary
 end 
 def word_substituter(tweet)
   
-  tweet.split(" ").collect do |word|
+  (tweet.split(" ").collect do |word|
     if dictionary.keys.include?(word.downcase) 
       word = dictionary[word.downcase]
     else 
       word
     end 
-  end.join(" ")
+  end).join(" ")
 end 
